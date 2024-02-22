92d6f17fbe5b1bb78744996fa7665079723cac1a
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-02-22 18:27:36 +0200

    add vercel.json

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,11 @@
-// src/Product.jsx
-
 // src/components/Product.jsx
 
-export const Product = (props) => {
+export const Product = ({ name, imgUrl, price }) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img src={props.imgUrl} alt={props.name} width="480" />
-      <p>Price: {props.price} credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
     </div>
   );
 };
